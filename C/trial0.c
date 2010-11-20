a0b69bd93cc73e2ea1545141a6d8eda469b99da6
Noufal Ibrahim <noufal@nibrahim.net.in>
2010-11-20 23:17:41 +0530

    Added first working version of trial file

--- C/trial0.c
+++ C/trial0.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "csv.h"
+#include "trial0.h"
+
+struct csv_parser parser; /* Declare the parser */
+
+void 
+parse_field(void *data, size_t len, void * dummy) 
+{
+  char * t = data;
+  printf("   %s\n",t);
+}
+
+void 
+parse_row(int val, void *dummy)
+{
+  printf("--------------------------------------------------\n");
+}
+  
+int
+main()
+{
+  FILE *fp;
+  char *data = NULL;
+  size_t processed_len;
+  data = (char *)malloc(sizeof(char) * FILE_SIZE);
+  memset(data, '\0', FILE_SIZE);
+
+  csv_init(&parser, CSV_APPEND_NULL);
+  fp = fopen(SAMPLE_FILE, "r");
+  fread(data, sizeof(char), FILE_SIZE, fp);
+
+  processed_len = csv_parse(&parser, data, FILE_SIZE, 
+			    parse_field,
+			    parse_row,
+			    NULL);
+  printf("\nParsed %d bytes\n",(int)processed_len);
+
+  return 0;
+}
---
+++ C/trial0.h
@@ -0,0 +1,5 @@
+const char *SAMPLE_FILE = "../extras/DMI_house_11-18-2010.csv";
+const int FILE_SIZE = 177081;
+
+void parse_field(void *, size_t, void *);
+void parse_row(int, void *);
