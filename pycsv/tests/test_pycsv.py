85ecf2f4926555d546266496eff337fc53804852
Noufal Ibrahim <noufal@nibrahim.net.in>
2010-11-21 23:48:41 +0530

    3: red

--- pycsv/tests/test_pycsv.py
+++ pycsv/tests/test_pycsv.py
@@ -8,5 +8,16 @@ def test_csvfile(setup):
     import pycsv
     with open("tests/sample.csv") as f:
         csvfile = pycsv.CSVFile(f)
+
+
+def test_csvfile_typeerror(setup):
+    "Try initilialising CSVFile without arguments. Should raise TypeError"
+    import pycsv
+    try:
+        csvfile = pycsv.CSVFile()
+        assert False, "Didn't raise TypeError when CSVFile instantiated without args"
+    except TypeError:
+        return
+            
     
 
