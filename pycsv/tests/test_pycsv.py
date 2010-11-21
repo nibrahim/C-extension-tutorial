619c7102e443a191845f9762ff565630c8ba21e1
Noufal Ibrahim <noufal@nibrahim.net.in>
2010-11-21 21:12:25 +0530

    2: red

--- pycsv/tests/test_pycsv.py
+++ pycsv/tests/test_pycsv.py
@@ -1,4 +1,12 @@
 def test_import(setup):
     "Try importing the module"
     import pycsv
+
+
+def test_csvfile(setup):
+    "Initialise a CSVFile object with a file and check the file "
+    import pycsv
+    with open("tests/sample.csv") as f:
+        csvfile = pycsv.CSVFile(f)
     
+
