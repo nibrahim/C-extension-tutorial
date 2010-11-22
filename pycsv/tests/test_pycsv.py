e12a7b2575de9ad82cefb81f6b366c721f34ae0c
Noufal Ibrahim <noufal@nibrahim.net.in>
2010-11-22 20:03:49 +0530

    4:red

--- pycsv/tests/test_pycsv.py
+++ pycsv/tests/test_pycsv.py
@@ -4,7 +4,7 @@ def test_import(setup):
 
 
 def test_csvfile(setup):
-    "Initialise a CSVFile object with a file and check the file "
+    "Initialise a CSVFile object with a file"
     import pycsv
     with open("tests/sample.csv") as f:
         csvfile = pycsv.CSVFile(f)
@@ -19,5 +19,11 @@ def test_csvfile_typeerror(setup):
     except TypeError:
         return
             
-    
+def test_csvfile_storing_file(setup):
+    "Makes sure that the provided file like object is stored in the CSVFile for later use"
+    import pycsv
+    with open("tests/sample.csv") as f:
+        csvfile = pycsv.CSVFile(f)
+        assert csvfile._file == f, "Stored _file is not the one we created"
+        
 
